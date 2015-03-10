a05cd5e21fe7dd816bb51071cc76906a356fb83f
Simon Madsen <simon@smadsen.com>
2015-03-10 11:41:04 +0100

    BaseController
    base controller with viewBag

---
+++ src/common/Controllers/BaseController.php
@@ -0,0 +1,18 @@
+<?php namespace Common\Controllers;
+
+use Illuminate\Foundation\Bus\DispatchesCommands;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+
+class BaseController extends Illuminate\Routing\Controller
+{
+    use DispatchesCommands, ValidatesRequests;
+
+    protected $viewBag;
+
+    function __construct()
+    {
+        $this->viewBag = [];
+    }
+
+
+}
